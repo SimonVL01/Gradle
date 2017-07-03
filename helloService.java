57deb8279dd781462d3d44bfbb9d31b892b6223d
SimonVL01 <simon-vanleeuw@hotmail.com>
2017-07-03 10:41:33 +0200

    bericht

--- helloService.java
+++ helloService.java
@@ -3,7 +3,7 @@ package com.realdolmen;
 /**
  * Created by vdabcursist on 03/07/2017.
  */
-public class helloService {
+public class HelloService {
     public String sayHello(String name) {
         return name;
     }
