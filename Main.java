2a1499898206aa30313eec7c53c83eaa8873f247
SimonVL01 <simon-vanleeuw@hotmail.com>
2017-07-03 10:40:18 +0200

    bericht

---
+++ Main.java
@@ -0,0 +1,7 @@
+package com.realdolmen;
+
+/**
+ * Created by vdabcursist on 03/07/2017.
+ */
+public class Main {
+}
---
+++ helloService.java
@@ -0,0 +1,10 @@
+package com.realdolmen;
+
+/**
+ * Created by vdabcursist on 03/07/2017.
+ */
+public class helloService {
+    public String sayHello(String name) {
+        return name;
+    }
+}
